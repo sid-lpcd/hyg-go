22ddc2c15ac55aafb3f7c5a294b751d47625d4ee
Sid <sidoniomiguelsilva567@gmail.com>
2024-11-26 20:10:11 +0000

    Add save form component

---
+++ src/components/base/Form/Form.jsx
@@ -0,0 +1,91 @@
+import "./Form.scss";
+import Error from "../../../assets/icons/error-icon.svg?react";
+import { v4 as uuidv4 } from "uuid";
+
+export default function Form({
+  title,
+  labels,
+  formData,
+  errorData,
+  handleChange,
+  handleCancel,
+  handleSubmit,
+}) {
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h2 className="form__title">{title}</h2>
+      <div className="form__section">
+        {labels?.map((label) => {
+          let input = <></>;
+          switch (label.type) {
+            case "input":
+              input = (
+                <input
+                  name={label.name}
+                  value={formData[label.name]}
+                  onChange={(e) => handleChange(e)}
+                  type="text"
+                  className={`form__input${
+                    errorData[label.name] ? " form__input--error" : ""
+                  }`}
+                  placeholder={label.placeholder}
+                />
+              );
+              break;
+            case "textarea":
+              input = (
+                <textarea
+                  name={label.name}
+                  value={formData[label.name]}
+                  onChange={(e) => handleChange(e)}
+                  className={`form__textarea${
+                    errorData[label.name] ? " form__textarea--error" : ""
+                  }`}
+                  placeholder={label.placeholder}
+                />
+              );
+              break;
+            case "select":
+              input = (
+                <select
+                  name={label.name}
+                  value={formData[label.name]}
+                  onChange={(e) => handleChange(e)}
+                  className={`form__select${
+                    errorData[label.name] ? " form__select--error" : ""
+                  }`}
+                >
+                  {label.options?.map((option) => (
+                    <option value={option} key={uuidv4()}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              );
+            default:
+              break;
+          }
+          return (
+            <div className="form__row" key={label.name}>
+              <label className="form__label">{label.text}</label>
+              {input}
+              {errorData[label.name] && (
+                <p className="form__error">
+                  <Error /> This is a required field
+                </p>
+              )}
+            </div>
+          );
+        })}
+      </div>
+      <div className="form__btn-container">
+        <button className="form__btn" onClick={handleCancel}>
+          No
+        </button>
+        <button className="form__btn-active" onClick={handleSubmit}>
+          Yes
+        </button>
+      </div>
+    </form>
+  );
+}
--- src/pages/CreatePlanPage/CreatePlanPage.jsx
+++ src/pages/CreatePlanPage/CreatePlanPage.jsx
@@ -6,11 +6,47 @@ import Header from "../../components/sections/Header/Header";
 import BackArrowIcon from "../../assets/icons/back-arrow-icon.svg?react";
 import CloseIcon from "../../assets/icons/close-icon.svg?react";
 import InputText from "../../components/base/InputText/InputText";
+import axios from "axios";
+import Form from "../../components/base/Form/Form";
 
 const CreatePlanPage = () => {
   const [selected, setSelected] = useState("");
   const [locations, setLocations] = useState(null);
   const [open, setOpen] = useState(false);
+  const [tripData, setTripData] = useState({
+    user_id: 1,
+    title: "",
+    description: "",
+    location_id: null,
+    start_date: "",
+    end_date: "",
+  });
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
+  const [errorData, setErrorData] = useState({
+    title: false,
+    description: false,
+    location_id: false,
+    start_date: false,
+    end_date: false,
+  });
+
+  const labels = [
+    {
+      name: "title",
+      text: "Trip title",
+      type: "input",
+      placeholder: "Name your trip...",
+    },
+    {
+      name: "description",
+      text: "Description",
+      type: "textarea",
+      placeholder: "Describe your trip...",
+    },
+  ];
 
   const navigate = useNavigate();
 
@@ -18,11 +54,41 @@ const CreatePlanPage = () => {
   const onCloseModal = () => {
     setOpen(false);
   };
-  const goBack = () => navigate("/");
 
-  const saveTrip = async () => {
-    // save trip axios
-    goBack();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+    setErrorData({ ...errorData, [name]: false });
+  };
+
+  const handleCancel = () => {
+    navigate("/");
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    let hasErrors = false;
+    const newErrorData = { ...errorData };
+
+    Object.keys(formData).forEach((key) => {
+      if (!formData[key]) {
+        newErrorData[key] = true;
+        hasErrors = true;
+      }
+    });
+
+    setErrorData(newErrorData);
+    console.log(newErrorData);
+
+    if (hasErrors) return;
+    try {
+      const response = axios.post(
+        `${import.meta.env.VITE_HYGGO_API_URL}/plans/`,
+        { ...tripData, ...formData }
+      );
+    } catch (error) {}
+    navigate("/");
   };
 
   const getLocations = async () => {
@@ -44,7 +110,7 @@ const CreatePlanPage = () => {
           <CloseIcon onClick={onOpenModal} className="header__icon" />
         }
       />
-      <main>
+      <main className="main">
         <InputText
           isAutocomplete={true}
           options={locations}
@@ -55,18 +121,15 @@ const CreatePlanPage = () => {
       </main>
 
       <Modal open={open} onClose={onCloseModal} center>
-        <h3 className="react-responsive-modal__title">Save Dream?</h3>
-        <div className="react-responsive-modal__btn-container">
-          <button className="react-responsive-modal__btn" onClick={saveTrip}>
-            No
-          </button>
-          <button
-            className="react-responsive-modal__btn-active"
-            onClick={saveTrip}
-          >
-            Yes
-          </button>
-        </div>
+        <Form
+          title="Do you want to save this trip?"
+          labels={labels}
+          formData={formData}
+          errorData={errorData}
+          handleChange={handleChange}
+          handleCancel={handleCancel}
+          handleSubmit={handleSubmit}
+        />
       </Modal>
     </>
   );
